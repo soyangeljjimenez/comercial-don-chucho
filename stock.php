de96c574528490af3b73e7134a6aa1f561ea1435
Angel <estudio.angeljc@gmail.com>
2021-07-13 22:29:16 -0400

    Added: 	+Stock of products readed from JSON file. Deleted: 	-Files and folders innecesaries.

--- stock.php
+++ stock.php
@@ -5,16 +5,15 @@
 	P.F.G. Informatica para la Gestion Social - 3er semestre
 	Unidad Curricular: Laboratorio de Aplicaciones Web - Profesora: Marisela Alvarez
 	Estudiante: Angel J. Jimenez C. V-26921917
-	Archivo: index.html
+	Archivo: stock.php
 -->
-
 <!DOCTYPE html>
 <html lang="en">
 
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width-device-width, initial-scale=1.0">
-		<title>Comercial Don Chucho | Welcome</title>
+		<title>Comercial Don Chucho | Stock</title>
 		<link rel="stylesheet" type="text/css" href="./static/css/style.css">
 	</head>
 
@@ -32,7 +31,27 @@
 			</nav>
 
 			<div>
+				<table border="2">
+					<tbody>
+						<tr>
+							<th>Producto</th>
+							<th>Presentacion</th>
+							<th>Costo</th>
+						</tr>
+						<?php
+							$productsSTR = file_get_contents('./storage/products.json');
+							$productsPHP = json_decode($productsSTR, true);
 
+							foreach ($productsPHP as $products => $value) {
+								echo "<tr>";
+								foreach ($value as $key => $valor) {
+									echo "<td>$valor</td>";
+								}
+								echo "</tr>";
+							}
+						?>
+					</tbody>
+				</table>
 			</div>
 		</div>	
 
